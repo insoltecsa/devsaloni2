6c51281cc47538eda16eedbe31df29c60c405274
Byron Estrada <bestrada1981@gmail.com>
2021-10-25 15:21:00 -0600

    Comit reporte de ventas

--- custom/__manifest__.py
+++ custom/__manifest__.py
@@ -19,12 +19,13 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock'],
+    'depends': ['base','stock','sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/report_qwen_delivery.xml',
         'views/report_qwen_sales_coti.xml',
+        'views/report_tree_ventas.xml',
     ]
 }
