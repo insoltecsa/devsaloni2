85a684ea0941235063cb031c0096a7c88a041cd1
Byron Estrada <bestrada1981@gmail.com>
2021-10-25 15:53:32 -0600

    Comit reporte de ventas

--- custom/__manifest__.py
+++ custom/__manifest__.py
@@ -26,5 +26,6 @@
         # 'security/ir.model.access.csv',
         'views/report_qwen_delivery.xml',
         'views/report_qwen_sales_coti.xml',
+        'views/report_tree_ventas.xml',
     ]
 }
